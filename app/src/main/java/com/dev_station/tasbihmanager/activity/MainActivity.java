683e1e69a3942c34086c0355c29d45fe130e8186
Shekhar Ullah <ullah.shekhar@gmail.com>
2017-05-20 07:27:09 +0600

    first commit

---
+++ app/src/main/java/com/dev_station/tasbihmanager/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dev_station.tasbihmanager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dev_station.tasbihmanager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
